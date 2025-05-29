f2cdb00e93752b8ff0fee1c84c9e84a92d0148c7
David Pérez <98660771+MichiMoments@users.noreply.github.com>
2025-05-29 14:50:30 -0500

    It actually works right now

--- hardhat-contracts/utils/run-blockchain.py
+++ hardhat-contracts/utils/run-blockchain.py
@@ -12,7 +12,10 @@ docker_command = "docker compose up"
 # Full command to open a new terminal window and run the commands
 #full_command = f'gnome-terminal -- bash -c "{cd_command} && {docker_command}; exec bash"' This command works only for linux
 # Command for windows
-full_command = f'wt -d "{os.path.abspath(blockchain_project)}" powershell -Command "docker compose up"'
+full_command = (
+    f'start powershell -NoExit -Command '
+    f'"cd \\"{blockchain_project}\\"; docker compose up"'
+)
 
 # Execute the command
 os.system(full_command)
